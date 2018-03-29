c43b3b52567f1bc4b79afc273f08b3474bbe3e30
kawannam <kendrasemail@shaw.ca>
2018-03-29 09:19:32 -0600

    Initial commit

---
+++ Tardiness/Data.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tardiness
+{
+    /// <summary>
+    /// Interaction logic for Data.xaml
+    /// </summary>
+    public partial class Data : UserControl
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+
+        public void Update(int[] values)
+        {
+            point1.Margin = new Thickness(5, calcHeight(values[0]), 0, 0);
+            point2.Margin = new Thickness(15, calcHeight(values[1]), 0, 0);
+            point3.Margin = new Thickness(25, calcHeight(values[2]), 0, 0);
+
+            point4.Margin = new Thickness(35, calcHeight(values[3]), 0, 0);
+            point5.Margin = new Thickness(45, calcHeight(values[4]), 0, 0);
+            point6.Margin = new Thickness(55, calcHeight(values[5]), 0, 0);
+
+            point7.Margin = new Thickness(65, calcHeight(values[6]), 0, 0);
+            point8.Margin = new Thickness(75, calcHeight(values[7]), 0, 0);
+        }
+
+        private int calcHeight(int v)
+        {
+            return Math.Min(Math.Max((45 + (v / 2)), 0), 100);
+        }
+    }
+}
---
+++ Tardiness/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Tardiness
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int numberOfCycles = 100;
+        int[][][] chrom;
+        int[] truth;
+        public MainWindow()
+        {
+            InitializeComponent();
+            readResults();
+            Truth.Update(truth);
+            updateUiElements(30);
+        }
+
+        public void updateUiElements(int i)
+        {
+            Data1.Update(chrom[i][0]);
+            Data2.Update(chrom[i][1]);
+            Data2.Update(chrom[i][2]);
+            Data2.Update(chrom[i][3]);
+            Data2.Update(chrom[i][4]);
+            Data2.Update(chrom[i][5]);
+            Data2.Update(chrom[i][6]);
+            Data2.Update(chrom[i][7]);
+            Data2.Update(chrom[i][8]);
+        }
+
+        public void readResults()
+        {
+            chrom = new int[numberOfCycles][][];
+            truth = new int[9];
+
+            Console.WriteLine(Environment.CurrentDirectory + "/../../gep.csv");
+            using (var reader = new StreamReader( Environment.CurrentDirectory + "/../../gep.csv"))
+            {
+                int i = 0;
+                if (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    for (int v = 0; v < values.Length; v++ )
+                    {
+                        truth[v] = (int)float.Parse(values[v]);
+                    }
+                }
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    int l = 0;
+                    chrom[i] = new int[9][];
+                    for (int j = 0; j < 9; j++) {
+                        chrom[i][j] = new int[8];
+                        for (int k = 0; k <  8; k++)
+                        {
+                            chrom[i][j][k] = (int)float.Parse(values[l]);
+                            l++;
+                            if (l > 71)
+                            {
+                                Console.WriteLine(l);
+                            }
+                        }
+                    }
+                    i++;
+                }
+            }
+
+
+        }
+    }
+}
